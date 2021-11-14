ad178891cbf7e12c7f71f50b4a464ef6799d5d7d
Rendi Nicolas Mahendra <68139718+mahendrarendi@users.noreply.github.com>
2021-11-14 18:25:44 +0700

    Create Function_case_1.cpp

---
+++ Function_case_1.cpp
@@ -0,0 +1,17 @@
+/* program dua puluh empat */
+/* program sederhana menggunakan fungsi yang tidak mengembalikan nilai*/
+
+#include <iostream>
+#include<stdio.h> 
+#include<conio.h> 
+
+void garis();
+main() {
+   clrscr();
+   garis();
+   printf("NO NIM NAMA NILAI \n");
+   getch();
+}
+void garis(){
+    printf("======================= \n");
+}
